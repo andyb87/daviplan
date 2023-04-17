6230807338fd6b812cd1081a9aa37d2ff3e52c5e
andyb87 <63102372+andyb87@users.noreply.github.com>
2023-04-17 15:30:05 +0200

    Update createsuperuser.sh

--- dist/createsuperuser.sh
+++ dist/createsuperuser.sh
@@ -1,7 +1,7 @@
 # split sub-directory this file is in
 DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 SUBDIR=(${DIR//// })
-# name of container is sub-directory + _web_1 if no name is given to docker-compose
-CONTAINER=${SUBDIR[-1]}_web_1
+# name of container is sub-directory + -web-1 if no name is given to docker-compose
+CONTAINER=${SUBDIR[-1]}-web-1
 # call django command to create superuser insiode container
 docker exec -it $CONTAINER python manage.py createsuperuser
